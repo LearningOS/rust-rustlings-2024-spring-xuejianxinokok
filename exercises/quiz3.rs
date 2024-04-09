d26d8a775acfbe0e04801a356e12078f7fdcf599
xuejianxinokok <xuejianxinokok@163.com>
2024-04-09 09:55:34 +0800

    第二次提交

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub struct ReportCard {
     pub grade: f32,
@@ -28,6 +27,12 @@ impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+}
+use std::fmt;
+impl fmt::Display for ReportCard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, "A+")
     }
 }
 
@@ -57,7 +62,7 @@ mod tests {
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.to_string(), //.print()
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
