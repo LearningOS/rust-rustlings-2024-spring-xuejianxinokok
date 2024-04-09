b2df5aace8c92f81d74cac65df031a221c1b29d0
xuejianxinokok <xuejianxinokok@163.com>
2024-04-09 10:43:36 +0800

    2222

--- exercises/iterators/iterators3.rs
+++ exercises/iterators/iterators3.rs
@@ -9,7 +9,6 @@
 // Execute `rustlings hint iterators3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
@@ -31,7 +30,7 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
     }else if a%b==0 {
         Ok(a/b)
     }else{
-        Err(DivisionError::NotDivisible(NotDivisibleError{dividend:b,divisor:b}))
+        Err(DivisionError::NotDivisible(NotDivisibleError{dividend:a,divisor:b}))
     }
 
 }
@@ -41,7 +40,7 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>,DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
+    let division_results = numbers.into_iter().map(|n| divide(n, 27)).filter_map(Result::ok).collect();
     Ok(division_results)
 }
 
@@ -50,7 +49,8 @@ fn result_with_list() -> Result<Vec<i32>,DivisionError> {
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
 fn list_of_results() -> Vec<Result<i32,DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
+    division_results
 }
 
 #[cfg(test)]
